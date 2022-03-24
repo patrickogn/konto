2bcfc369a2bb54f9b0d03ddc2603492cadca6604
Patrick Nielsen <cph-pn157@cphbusiness.dk>
2022-03-24 11:19:56 +0100

    tilføjet at bruger kan hæve

---
+++ src/main/java/TransaktionServlet.java
@@ -0,0 +1,30 @@
+import DomæneObjekter.Konto;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "TransaktionServlet", value = "/TransaktionServlet")
+public class TransaktionServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        String beløb = request.getParameter("beløb");
+
+        Konto konto = (Konto) request.getSession().getAttribute("konto");
+
+        int i = Integer.parseInt(beløb);
+
+        int nysaldo = konto.hæv(i);
+
+        request.getRequestDispatcher("WEB-INF/BrugerSide.jsp").forward(request,response);
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
--- src/main/webapp/WEB-INF/BrugerSide.jsp
+++ src/main/webapp/WEB-INF/BrugerSide.jsp
@@ -11,10 +11,32 @@
     <title>Din side</title>
 </head>
 <body>
+<h1>Polish Workers inc</h1>
+<br>
 
-<h1>Velkommen du er nu logget ind på din konto! ${sessionScope.konto.navn}</h1>
+<h2>Velkommen:  ${sessionScope.konto.navn}</h2>
 
 <h2>Din saldo er: ${sessionScope.konto.saldo}</h2>
+
+
+<!-- log ud knap-->
+<%--<form action="LoginServlet">
+    <label for="fname">Navn:</label><br>
+    <input type="text" id="fname" name="navn" value="navn"><br>
+    <label for="lname">Kodeord:</label><br>
+    <input type="password" id="lname" name="kode" value="hemmelig"><br><br>
+    <input type="submit" value="Log af">
+    <br>--%>
+
+
+<h1>Hæv penge</h1>
+<br>
+
+<form action="TransaktionServlet">
+    <label for="fname">Angiv beløb</label><br>
+    <input type="number" id="fname" name="beløb" value="0"><br>
+    <input type="submit" value="Submit">
+</form>
 
 
 </body>
